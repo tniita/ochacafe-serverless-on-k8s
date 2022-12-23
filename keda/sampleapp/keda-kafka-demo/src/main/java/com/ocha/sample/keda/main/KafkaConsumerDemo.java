30d298568a5681a828ac8553f8132a080d75387f
tniita <takuya.niita@oracle.com>
2022-12-23 10:36:01 +0000

    initial commit

---
+++ keda/sampleapp/keda-kafka-demo/src/main/java/com/ocha/sample/keda/main/KafkaConsumerDemo.java
@@ -0,0 +1,18 @@
+package com.ocha.sample.keda.main;
+
+import java.util.logging.Logger;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+
+@ApplicationScoped
+public class KafkaConsumerDemo {
+
+    private final static Logger logger = Logger.getLogger(KafkaConsumerDemo.class.getSimpleName());
+
+    @Incoming("keda-demo")
+    public void consume(String payload) {
+        logger.info("message received!! " + payload);
+    }
+}
